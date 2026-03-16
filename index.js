a992bb7ad58a09b89f2ef582dd1fb13401735d08
Ruk Doe <info@doany.io>
2026-03-16 18:02:19 +0900

    定期実行追加

--- index.js
+++ index.js
@@ -1,56 +1,59 @@
 const fs = require("fs");
 const { Builder, Browser, By, until } = require("selenium-webdriver");
 
-(async () => {
-
+async function main() {
+  
   const [id, password, chassis] = fs
-    .readFileSync("parameter.txt","utf8")
-    .split(/\r?\n/)
-    .map(v => v.trim());
-
+  .readFileSync("parameter.txt","utf8")
+  .split(/\r?\n/)
+  .map(v => v.trim());
+  
   const driver = await new Builder().forBrowser(Browser.CHROME).build();
-
+  
   try {
-
+    
+    console.log("実行:", new Date());
+    
     await driver.get("https://www.reserve.naltec.go.jp/web/ap-entry?slinky___page=forward:slinkyLogin");
-
+    
     await driver.findElement(By.name("account_id")).sendKeys(id);
     await driver.findElement(By.name("password")).sendKeys(password);
     await driver.findElement(By.css('button[type="submit"]')).click();
-
+    
     await driver.wait(until.urlContains("A1021_01"),10000);
-
+    
     await driver.get("https://www.reserve.naltec.go.jp/web/ap-entry?slinky___page=forward:A1101_01");
-
+    
     await driver.findElement(By.id("insp_type_5")).click();
     await driver.findElement(By.id("insp_vehicle_class_1")).click();
     await driver.findElement(By.css('button[type="submit"]')).click();
-
+    
     await driver.findElement(By.css('button[data-topagename="A1101_03"]')).click();
-
+    
     const specified = await driver.findElements(
       By.xpath("//tr[th[contains(text(),'3月23日')]]/td[1]//button")
     );
-
+    
     if (specified.length) {
       await specified[0].click();
     } else {
       const available = await driver.findElements(
         By.css('button[data-topagename="A1101_04"]')
       );
-
+      
       if (!available.length) return;
-
+      
       await available.at(-1).click();
     }
-
+    
     await driver.findElement(By.name("chassis_no")).sendKeys(chassis);
     await driver.findElement(By.id("compliance_check")).click();
     await driver.findElement(By.css('button[data-topagename="A1101_05"]')).click();
     await driver.findElement(By.css('button[data-topagename="A1101_06"]')).click();
-
+    
   } finally {
     await driver.quit();
   }
-
-})();
+  
+};
+setInterval(main, 60 * 1000);
