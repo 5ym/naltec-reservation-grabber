c68e6362c623e001d4fbb7863d3ac15a09524805
Ruk Doe <info@doany.io>
2026-03-17 20:06:32 +0900

    予約変更機能追加

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const chrome = require("selenium-webdriver/chrome");
 const { Builder, Browser, By, until } = require("selenium-webdriver");
 
 async function main() {
+  // 事務所番号42:袖ヶ浦
+  const locationNumber = '41'
   
   const [id, password, chassis] = fs
   .readFileSync("parameter.txt","utf8")
@@ -18,23 +20,52 @@ async function main() {
   const driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build();
   
   try {
-    
     console.log("実行:", new Date());
-    
+    // ログイン
     await driver.get("https://www.reserve.naltec.go.jp/web/ap-entry?slinky___page=forward:slinkyLogin");
     await driver.findElement(By.name("account_id")).sendKeys(id);
     await driver.findElement(By.name("password")).sendKeys(password);
     await driver.findElement(By.css('button[type="submit"]')).click();
-    await driver.wait(until.urlContains("A1021_01"),10000);
     
-    await driver.get("https://www.reserve.naltec.go.jp/web/ap-entry?slinky___page=forward:A1101_01");
-    await driver.findElement(By.id("insp_type_5")).click(); // 1継続,5構変
+    // 予約画面
+    const reserveButton = await driver.wait(
+      until.elementLocated(By.css('a[data-topagename="A1101_01"]')),
+      10000
+    );
+    // 予約済みの場合変更画面へ行く
+    const reservedButton = await driver.findElements(
+      By.xpath(`//tr[td[5][contains(text(),"${chassis}")]]//button`)
+    );
+    let examDate, round;
+    if (reservedButton.length) {
+      await reservedButton[0].click();
+      const chnageButton = await driver.wait(
+        until.elementLocated(By.css('button[value="Forward:A1131_01"]')),
+        10000
+      )
+      examDate = await driver.findElement(By.xpath(
+        '//th[normalize-space(.)="受検日"]/following-sibling::td'
+      )).getText();
+      round = await driver.findElement(By.xpath(
+        '//th[normalize-space(.)="ラウンド"]/following-sibling::td'
+      )).getText();
+      chnageButton.click();
+    } else {
+      reserveButton.click();
+    }
+
+    // 種別画面
+    const inspectionType = await driver.wait(
+      until.elementLocated(By.id("insp_type_4")), // 1継続,4構変
+      10000
+    );
+    inspectionType.click();
     await driver.findElement(By.id("insp_vehicle_class_1")).click();
-    await driver.findElement(By.css('button[type="submit"]')).click();
+    await driver.findElement(By.xpath('(//button)[last()]')).click();
     
     // 事務所一覧が表示されるのを待つ
     const location = await driver.wait(
-      until.elementLocated(By.css('button[value="Forward:A1101_03,office=41"]')),
+      until.elementLocated(By.css(`button[value$="office=${locationNumber}"]`)),
       10000
     );
     location.click();
@@ -51,31 +82,66 @@ async function main() {
       await specified[0].click();
     } else {
       const available = await driver.findElements(
-        By.css('button[data-topagename="A1101_04"]')
+        By.css('button[value^="Forward"]')
       );
       if (!available.length) return;
       await available.at(-1).click();
     }
     
-    // 車両情報入力画面が出るのを待つ
-    const chassisInput = await driver.wait(
-      until.elementLocated(By.name("chassis_no")),
-      10000
-    );
-    await driver.wait(until.elementIsVisible(chassisInput), 10000);
-    chassisInput.sendKeys(chassis);
-    await driver.findElement(By.id("compliance_check")).click();
-    // 早すぎると画像認証が表示されるため少し待つ1.8秒
-    await driver.sleep(1800);
-    await driver.findElement(By.css('button[data-topagename="A1101_05"]')).click();
+    // 予約変更の場合次のボタンが表示されるまでまつ、予約の場合は車両情報入力画面が出るのを待つ
+    if (reservedButton.length) {
+      await driver.wait(
+        until.elementLocated(By.css('button[value^="Forward"]')),
+        10000
+      );
+      const afterExamDate = await driver.findElement(By.xpath(
+        '//th[normalize-space(.)="受検日"]/following-sibling::td'
+      )).getText();
+      const afterRound = await driver.findElement(By.xpath(
+        '//th[normalize-space(.)="ラウンド"]/following-sibling::td'
+      )).getText();
+      if (examDate === afterExamDate && round === afterRound) {
+        return;
+      }
+    } else {
+      // 車両情報入力画面が出るのを待つ
+      const chassisInput = await driver.wait(
+        until.elementLocated(By.name("chassis_no")),
+        10000
+      );
+      await driver.wait(until.elementIsVisible(chassisInput), 10000);
+      chassisInput.sendKeys(chassis);
+      await driver.findElement(By.id("compliance_check")).click();
+    }
+    await driver.findElement(By.css('button[value^="Forward"]')).click();
 
     // 予約内容確認画面が表示されるのを待つ
-    const confirmButton = await driver.wait(
-      until.elementLocated(By.css('button[data-topagename="A1101_06"]')),
+    await driver.wait(
+      until.elementLocated(By.css('button[value^="Forward"]')),
       10000
     );
-    confirmButton.click();
-    
+    // 画像認証が出た場合bot判定が消えるまでリロードする
+    for (let i = 0; i < 20; i++) {
+      const imageAuth = await driver.findElements(By.name("image_auth"));
+      if (!imageAuth.length) {
+        // 画像認証が無ければ抜ける
+        break;
+      }
+      console.log(`画像認証検出: ${i + 1}回目 リロードします`);
+      await driver.navigate().refresh();
+      // ページ読み込み待ち
+      await driver.sleep(100);
+    }
+    // クリック前にもう一度確認ボタンがあるか確認する
+    const lastConfirmButton = await driver.wait(
+      until.elementLocated(By.css('button[value^="Forward"]')),
+      10000
+    );
+    lastConfirmButton.click();
+    await driver.wait(
+      until.elementLocated(By.xpath('//span[contains(text(),"予約番号")]')),
+      10000
+    );
   } catch (error) {
     console.error(error);
   } finally {
